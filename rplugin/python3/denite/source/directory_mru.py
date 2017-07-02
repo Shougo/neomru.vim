6ad4b29fac02e48ee4a02a479d0c7b0a815f01d9
Jacob Niehus <jacob.niehus@gmail.com>
2017-07-02 11:16:25 -0700

    Set default_action=cd for directory_mru kind

--- rplugin/python3/denite/source/directory_mru.py
+++ rplugin/python3/denite/source/directory_mru.py
@@ -14,6 +14,7 @@ class Source(Base):
 
         self.name = 'directory_mru'
         self.kind = 'directory'
+        self.default_action = 'cd'
 
     def gather_candidates(self, context):
         return [{'word': x, 'action__path': x} for x
