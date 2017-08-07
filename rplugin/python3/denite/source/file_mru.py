56a09edd78fc0c279c5aff2a915a4b2b11df11b2
somini <dev@somini.xyz>
2017-08-07 21:01:11 +0100

    Add a way to specify abbr paths
    This introduces a new setting to `fnamemodify` file locations for
    display.
    
    Defaults to the old behaviour of not touching the paths.

--- rplugin/python3/denite/source/file_mru.py
+++ rplugin/python3/denite/source/file_mru.py
@@ -5,7 +5,6 @@
 # ============================================================================
 
 from .base import Base
-from denite.util import relpath
 
 
 class Source(Base):
@@ -16,12 +15,22 @@ class Source(Base):
         self.name = 'file_mru'
         self.kind = 'file'
         self.sorters = []
+        self.vars = {
+            'fnamemodify': ':~',
+        }
 
     def gather_candidates(self, context):
+        def time_format(x):
+            return self.vim.call('getftime', x)
+
+        def path_format(x):
+            return self.vim.call('fnamemodify', x, self.vars['fnamemodify'])
+
         return [{
-            'word': x,
-            'abbr': self.vim.call('neomru#_abbr', relpath(self.vim, x)),
+            'word': path_format(x),
+            'abbr': self.vim.call('neomru#_abbr',
+                path_format(x), time_format(x)),
             'action__path': x
         } for x in self.vim.eval(
             'neomru#_get_mrus().file.'
-            +'gather_candidates([], {"is_redraw": 0})')]
+            + 'gather_candidates([], {"is_redraw": 0})')]
